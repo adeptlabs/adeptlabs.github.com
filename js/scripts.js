cfd7a893f708321332da0783b5cf87d342d4b4f2
Adept Labs <129678793+adeptlabs@users.noreply.github.com>
2023-12-14 21:18:57 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 $(document).ready(function() {
 	"use strict";
 	$("#countdown").countdown({
-		date: "01 December 2023 9:00:00", /** Enter new date here **/
+		date: "18 December 2023 9:00:00", /** Enter new date here **/
 		format: "on"
 	},
 	function() {
